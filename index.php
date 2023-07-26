3c56c71f855494ae4d4be15d3a26f06ce819e19d
GOWTHAM SIVASUBRAMANIAM <gowthamsivasubramaniam98@gmail.com>
2023-07-26 23:01:23 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <title>whatsapp-web</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        
+    </head>
+    <body>
+        <div class="container">
+            <div class="leftside" >
+            <div class="header">
+                <div class="userimg">
+                    <img src="user.JPG" class="cover">
+                </div>
+                <ul class="nav">
+                    <li><ion-icon name="scan-circle-outline"></ion-icon></li>
+                    <li><ion-icon name="chatbox"></ion-icon></li>
+                    <li><ion-icon name="ellipsis-vertical-outline"></ion-icon> </li>
+                </ul>
+            </div>
+            <!--serch-->
+            <div class="search1">
+                <div>
+                    <input type="text" placeholder="Search or  start new chat">
+                    <ion-icon name="search-outline"></ion-icon>
+                </div>
+            </div>
+            <!--chhat list-->
+            <div
+            class="chatli">
+               <div class="block active">
+                <div class="imgbx">
+                    <img src="kavin.jfif" class="cover">
+                </div>
+                 <div class="details">
+                    <div class="listhead">
+                        <h4>Kavin</h4>
+                        <p class="time">10:56</p>
+                    </div>
+                    <div class="message_p">
+                        <p>HOW ARE YOU </p>
+                    </div>
+                 </div>
+               </div>
+               <div class="block ">
+                <div class="imgbx">
+                    <img src="muthu.jpg" class="cover">
+                </div>
+                 <div class="details">
+                    <div class="listhead">
+                        <h4>Muthu</h4>
+                        <p class="time">10:56</p>
+                    </div>
+                    <div class="message_p">
+                        <p>hi da</p>
+                    </div>
+                 </div>
+               </div>
+               <div class="block">
+                <div class="imgbx">
+                    <img src="mano.webp" class="cover">
+                </div>
+                 <div class="details">
+                    <div class="listhead">
+                        <h4>Mano</h4>
+                        <p class="time"> 11:57</p>
+                    </div>
+                    <div class="message_p">
+                        <p>hi</p>
+                    </div>
+                 </div>
+               </div>
+               <div class="block">
+                <div class="imgbx">
+                    <img src="kalai.webp" class="cover">
+                </div>
+                 <div class="details">
+                    <div class="listhead">
+                        <h4>Kalai</h4>
+                        <p class="time">10:56</p>
+                    </div>
+                    <div class="message_p">
+                        <p>hey</p>
+                    </div>
+                 </div>
+               </div>
+               <div class="block unread">
+                <div class="imgbx">
+                    <img src="gayathri.jpg" class="cover">
+                </div>
+                 <div class="details">
+                    <div class="listhead">
+                        <h4>Gayathri</h4>
+                        <p class="time">10:56</p>
+                    </div>
+                    <div class="message_p">
+                        <p>hello</p>
+                        <b>1</b>
+                    </div>
+                 </div>
+               </div>
+               <div class="block unread">
+                <div class="imgbx">
+                    <img src="muthu.jpg" class="cover">
+                </div>
+                 <div class="details">
+                    <div class="listhead">
+                        <h4>Gobi</h4>
+                        <p class="time">10:56</p>
+                    </div>
+                    <div class="message_p">
+                        <p>hello</p>
+                        <b>1</b>
+                    </div>
+                 </div>
+               </div>
+               <div class="block unread">
+                <div class="imgbx">
+                    <img src="kavin.jfif" class="cover">
+                </div>
+                 <div class="details">
+                    <div class="listhead">
+                        <h4>Hari</h4>
+                        <p class="time">10:56</p>
+                    </div>
+                    <div class="message_p">
+                        <p>hello</p>
+                        <b>1</b>
+                    </div>
+                 </div>
+               </div>
+               <div class="block unread">
+                <div class="imgbx">
+                    <img src="kalai.webp" class="cover">
+                </div>
+                 <div class="details">
+                    <div class="listhead">
+                        <h4>Sundar</h4>
+                        <p class="time">10:56</p>
+                    </div>
+                    <div class="message_p">
+                        <p>hello</p>
+                        <b>1</b>
+                    </div>
+                 </div>
+               </div>
+               <div class="block unread">
+                <div class="imgbx">
+                    <img src="back.jpg" class="cover">
+                </div>
+                 <div class="details">
+                    <div class="listhead">
+                        <h4>Vijay</h4>
+                        <p class="time">10:56</p>
+                    </div>
+                    <div class="message_p">
+                        <p>hello</p>
+                        <b>1</b>
+                    </div>
+                 </div>
+               </div>
+               <div class="block unread">
+                <div class="imgbx">
+                    <img src="daya.jpg" class="cover">
+                </div>
+                 <div class="details">
+                    <div class="listhead">
+                        <h4>Dayanithi</h4>
+                        <p class="time">10:56</p>
+                    </div>
+                    <div class="message_p">
+                        <p>hello</p>
+                        <b>1</b>
+                    </div>
+                 </div>
+               </div>
+               
+            </div>
+
+            </div>
+
+            <div class="right">
+              <div class="header">
+                <div class="imgtxt">
+                    <div class="userimg">
+                        <img src="kavin.jfif" class="cover">
+                    </div>
+                    <h4> Kavin <br><span>Online</span></h4>
+                </div>
+                <ul class="nav">
+                    <li><ion-icon name="search-outline"></ion-icon></li>
+                    <li><ion-icon name="ellipsis-vertical-outline"></ion-icon> </li>
+                </ul>
+              </div>
+                
+               <!--chat start-->
+             <div class="chatbox">
+                <div class="msg my_msg">
+                 <p>Hii<br><span>12:15</span></p>
+                </div> 
+
+                 <div class="msg frnd_msg">
+                <p>Hello<br><span>12:17</span></p>
+             </div> 
+              <div class="msg frnd_msg">
+                <p>Wikis are enabled by wiki software, otherwise known as wiki engines. A wiki engine, being a form of a content management system, differs from other web-based systems such as blog software, in that the content is created without any defined owner or leader, and wikis have little inherent structure, allowing structure to emerge according to the needs of the users.[1] Wiki engines usually allow content to be written using<br><span>12:17</span></p>
+            </div> 
+ 
+             </div>
+             <!--chat end-->
+           <div class="chat_inp">
+            <ion-icon name="happy-outline"></ion-icon>
+            <ion-icon name="attach-outline"></ion-icon>
+            <input type="text " placeholder="Type a message">
+            <ion-icon name="send-outline"></ion-icon>
+            <ion-icon name="mic-outline"></ion-icon>
+           </div>
+            </div>
+           
+        </div>
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    </body>
+</html>
